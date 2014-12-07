bc3d3efb7495a5b671eb4adb0676b9f7ef993a8e
Sam Cordingley <samcordingley@gmail.com>
2014-12-07 15:24:19 +0000

    adds google maps test

--- test-app/www/js/index.js
+++ test-app/www/js/index.js
@@ -1,36 +1,22 @@
+var map;
+function initialize() {
+    // Create a simple map.
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: {lat: 51.5, lng: -3}
+    });
 
+    $.getJSON( "test.geojson", function( data ) {
+        map.data.addGeoJson(data);
+    });
+    // Load a GeoJSON from the same server as our demo.
+    var featureStyle = {
+        fillColor: 'green',
+        visible: true,
+        strokeWeight: 1
+    };
+    map.data.setStyle(featureStyle);
 
-var url = 'http://www.geostore.com/OGC/OGCInterface?SERVICE=WFS&' +
-'UID=UDATAGOV2011&PASSWORD=datagov2011&INTERFACE=ENVIRONMENTWFS&VERSION=2.0.0&LC=4000&' +
-'request=GetFeature&typeNames=ENVIRONMENTWFS:flood_warning_areas_010k_inspire&count=20';
+}
 
-
-var vector = new ol.layer.Vector({
-  source: new ol.source.StaticVector({
-    format: new ol.format.WFS({
-      featureNS: 'http://192.168.84.58:8090/ENVIRONMENTWFS',
-      featureType: 'geometry'
-    }),
-    projection: 'EPSG:4326',
-    url: url
-  })
-})
-
-var raster = new ol.layer.Tile({
-
-  preload: Infinity,
-    source: new ol.source.BingMaps({
-      key: 'Ak-dzM4wZjSqTlzveKz5u0d4IQ4bRzVI309GxmkgSVr1ewS6iPSrOvOKhA-CJlm3',
-      imagerySet: 'Aerial'
-    })
-
-});
-
-var map = new ol.Map({
-  layers: [raster, vector],
-  target: document.getElementById('map'),
-  view: new ol.View({
-          center: ol.proj.transform([37.41, 8.82], 'EPSG:4326', 'EPSG:3857'),
-          zoom: 4
-        })
-});
+google.maps.event.addDomListener(window, 'load', initialize);
