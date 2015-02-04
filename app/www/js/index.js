4768a4162888e7e27b8ee55a8311620a8518b681
Sam Cordingley <samcordingley@gmail.com>
2015-02-04 14:20:33 +0000

    fix slider on android

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,6 +1,6 @@
 // MAP FUNCTIONS
 var map;
-  
+
 //https://www.mapbox.com/developers/api/
 var accToken = '?access_token=pk.eyJ1IjoibWMxMzgxOCIsImEiOiI4Tlp2cFlBIn0.reMspV4lEYawDlSZ6U1fqQ';
 map = L.map('map-layer', {
@@ -58,18 +58,18 @@ $('#search-bar input').click(function(){
 
     // IF HIDDEN, SHOW
     if(!$('#search-bar-expanded').is(':visible')){
-        
+
         addrSearch();
-        
+
         $('#search-mask').fadeIn();
-        
+
         // SLIDE DOWN
         $('#search-bar-expanded').slideDown();
 
         // EXCHANGE ICON TO GO BACK
         $('#search-icon img').fadeOut(100, function(){
             $(this).attr('src', 'img/search/left-arrow-icon.png').fadeIn();
-        });      
+        });
     }
 
 });
@@ -111,16 +111,16 @@ function buttonExpand(side, override){
     else {
         $('#' + side + '-legend-button').removeClass("btn-expanded");
         $('#' + side + '-topic-button').removeClass("btn-expanded");
-        
+
         setTimeout(function(){
             $('#' + side + '-legend-button').hide();
             $('#' + side + '-topic-button').hide();
         }, 500);
-        
+
         $('#' + side + '-button').attr('src', 'img/buttons/'+ side +'-menu-icon.png');
     }
 }
-    
+
 $('#left-button').click(function() {
     buttonExpand("left");
 });
@@ -136,7 +136,7 @@ function getTransform() {
     var results = $('.leaflet-map-pane').css('transform').match(/matrix\((-?\d+), ?(-?\d+), ?(-?\d+), ?(-?\d+), ?(-?\d+), ?(-?\d+)\)/);
 
     if(!results) return [0, 0];
-    
+
     return results.slice(5, 7);
 }
 
@@ -152,55 +152,55 @@ function adjustDataContainer(){
     se = map.containerPointToLayerPoint(map.getSize()),
     range = sliderOffset / $(window).width(),
     clipX = nw.x + (se.x - nw.x) * range;
-    
+
     $('#leftData').css("clip", 'rect(' + [nw.y, clipX, se.y, nw.x].join('px,') + 'px)');
     $('#rightData').css("clip", 'rect(' + [nw.y, se.x, se.y, clipX].join('px,') + 'px)');
 }
-   
+
 map.on('move', adjustDataContainer);
 
 function generateButtonRules(){
     if($(window).width() <= 768) return {buttonLimit: 80};
-    else if($(window).width() <= 1024) return {buttonLimit: 120}; 
+    else if($(window).width() <= 1024) return {buttonLimit: 120};
     else return {buttonLimit: 150};
 }
 var sliderLimits = generateButtonRules();
-    
+
 function offsetFunc(){
-    
+
     adjustDataContainer();
-    
+
     // SHOW BUTTON IF SIDE IS VISIBLE
     if(sliderOffset >= sliderLimits.buttonLimit && !$('#left-button-block').is(':visible')){
         $('#left-button-block').fadeIn();
     }
     else if(sliderOffset < sliderLimits.buttonLimit && $('#left-button-block').is(':visible')){
         buttonExpand("left", true);
-        $('#left-button-block').fadeOut();        
+        $('#left-button-block').fadeOut();
     }
     else if(sliderOffset <= $(window).width() - sliderLimits.buttonLimit && !$('#right-button-block').is(':visible')){
         $('#right-button-block').fadeIn();
     }
     else if(sliderOffset > $(window).width() - sliderLimits.buttonLimit && $('#right-button-block').is(':visible')){
         buttonExpand("right", true);
-        $('#right-button-block').fadeOut();    
+        $('#right-button-block').fadeOut();
     }
-    
+
     // SHOW ARROWS ON SIDE OF SLIDER
     if(sliderOffset >= 50 && $('#drag-right').is(':visible')){
         $('#drag-right').fadeOut();
     }
     else if(sliderOffset < 50 && !$('#drag-right').is(':visible')){
-        $('#drag-right').fadeIn();        
+        $('#drag-right').fadeIn();
     }
     if(sliderOffset <= $(window).width() - 100 && $('#drag-left').is(':visible')){
         $('#drag-left').fadeOut();
     }
     else if(sliderOffset > $(window).width() - 100 && !$('#drag-left').is(':visible')){
-        $('#drag-left').fadeIn();        
+        $('#drag-left').fadeIn();
     }
 }
-    
+
 // DRAGGABLE SLIDER
 $('#slider-bar').on('mousedown touchstart', function(){
     $(this).addClass('dragging');
@@ -212,24 +212,26 @@ $('#slider-bar').on('mouseup touchend', function(){
 });
 
 $('body').on('mousemove touchmove', function(e){
-    
+
     if(e.type == "touchmove") var out = e.originalEvent.touches[0];
     else var out = e;
-        
-    if($('#slider-bar').hasClass('dragging')){      
-  
+
+    if($('#slider-bar').hasClass('dragging')){
+
         if(sliderOffset != out.pageX);{
-        
+
             if(sliderOffset < 0) sliderOffset = 0;
             else if(sliderOffset > $(window).width()) sliderOffset = $(window).width();
             else sliderOffset = out.pageX;
-            
+
             offsetFunc();
             $('#slider-bar').offset({
                 left: sliderOffset - 25
             });
         }
     }
+
+    e.preventDefault();
 });
 
 // ADDRESS SEARCH
@@ -238,21 +240,21 @@ var searchTimer;
 
 //search bar text changed
 $('#search-input').on('input', function() {
-    
+
     //reset fav icon & curloc
     $('#search-bar-favourite img').attr("src","img/search/favourite-empty-icon.png");
     curLocData = undefined;
-    
+
     addrSearch();
 });
 
 //Update the dropdown box with the search results (given by 'data')
 function addrSearch() {
-    
+
     // no API call if empty
     var inp = $("#search-input");
     $('#search-results .expanded-locations').empty();
-    
+
     if(inp.val() == ""){
         $('#search-results').hide();
         $('#search-bar-empty').hide();
@@ -262,17 +264,17 @@ function addrSearch() {
         $('#search-results').show();
         $('#search-bar-empty').css("display", "inline-block");
     }
-    
+
     //timer used so fast-typing doesn't trigger rapid requests
     clearTimeout(searchTimer);
-    searchTimer = setTimeout(function() {      
+    searchTimer = setTimeout(function() {
         $.ajax({
             //fetch from http://nominatim.openstreetmap.org/
             url: 'http://nominatim.openstreetmap.org/search?format=json&limit=5&countrycodes=gb&q=' + inp.val(),
             dataType: 'json',
             success: updateSearchResults
         });
-    }, 250); 
+    }, 250);
 }
 
 function updateSearchResults(data){
@@ -281,7 +283,7 @@ function updateSearchResults(data){
     }
     var items = [];
     var displayedResults = [];
-    
+
     $.each(data, function(key, val) {
         if($.inArray(val.display_name.toLowerCase(), displayedResults) == -1){
             items.push("<li data-lat='"+val.lat+"' data-lon='"+val.lon+"' data-type='"+val.type+"'>" + val.display_name +'</li>');
@@ -306,14 +308,14 @@ localStorage.setItem("savedLocations", JSON.stringify(savedLocations));
 // add saved location
 function addSavedLocation(){
     var val = savedLocations[savedLocations.length - 1];
-    $('#saved-locations .expanded-locations').append("<li data-lat='"+val.lat+"' data-lon='"+val.lng+"' data-type='"+val.type+"'><div>" + val.dispname + 
+    $('#saved-locations .expanded-locations').append("<li data-lat='"+val.lat+"' data-lon='"+val.lng+"' data-type='"+val.type+"'><div>" + val.dispname +
         '</div><img class="favourite-delete" src="img/search/favourite-delete-icon.png" alt="Delete favourite" /><div class="clearfix"></div></li>');
-        
+
     if(savedLocations.length == 1){
         $('#saved-locations .expanded-title').text("Saved locations");
     }
 }
-    
+
 // remove saved location
 function removeSavedLocation(index){
     $('#saved-locations .expanded-locations li:eq(' + index + ')').remove();
@@ -321,18 +323,18 @@ function removeSavedLocation(index){
         $('#saved-locations .expanded-title').text("No saved locations");
     }
 }
-    
+
 //updates saved location ui with contents of 'savedLocations'
 function renderSavedLocations() {
-      
+
     var items = [];
     $.each(savedLocations, function(key, val) {
-        items.push("<li data-lat='"+val.lat+"' data-lon='"+val.lng+"' data-type='"+val.type+"'><div>" + val.dispname + 
+        items.push("<li data-lat='"+val.lat+"' data-lon='"+val.lng+"' data-type='"+val.type+"'><div>" + val.dispname +
             '</div><img class="favourite-delete" src="img/search/favourite-delete-icon.png" alt="Delete favourite" /><div class="clearfix"></div></li>');
     });
-    
+
     $('#saved-locations .expanded-locations').empty();
-    
+
     if (items.length != 0) {
         $(items.join('')).appendTo('#saved-locations .expanded-locations');
         $('#saved-locations .expanded-title').text("Saved locations");
@@ -364,11 +366,11 @@ function goToLocation(data) {
         dispname: $(data).text(),
         type: $(data).attr('data-type')
     };
-    
+
     var location = new L.LatLng(curLocData.lat, curLocData.lng);
     map.panTo(location);
     $('#search-input').val($(data).text());
-  
+
     //set zoom level based on type of location
     //known outliers: tadley = administrative?
     if(curLocData.type == "administrative") {  //country
@@ -398,14 +400,14 @@ function toggleFavourite() {
     }
     if(savedLocations.length == 0) {
         savedLocations.push(curLocData); addSavedLocation();
-    } else {  
+    } else {
         var index = savedLocIndex(curLocData.dispname);
         if(index != -1) {
             savedLocations.splice(index, 1); removeSavedLocation(index);
         } else {
             savedLocations.push(curLocData); addSavedLocation();
         }
-    } 
+    }
     toggleFavIcon();
     localStorage["savedLocations"] = JSON.stringify(savedLocations);
 }
@@ -446,31 +448,31 @@ $("#search-bar-empty").click(function(){
     $("#search-bar-empty").hide();
 });
 
-var datasetsArray = {   
+var datasetsArray = {
     "Flood warning areas" : "ea:flood_warning_areas",
     "Flood alert areas" : "ea:flood_alert_areas",
     "Nitrate-sensitive areas" : "ea:nitrate_sensitive_areas",
     "Areas of outstanding natural beauty" : "ea:areasoutstgnaturalbeauty_eng",
-    "Agricultural land" : "ea:agri_land_class",   
+    "Agricultural land" : "ea:agri_land_class",
     "Flood risk areas" : "ea:flood_risk_areas"
 };
 
 // TOGGLE MAP TOPIC MENU
 var menuViewed;
 $('#right-topic-button, #left-topic-button').click(function() {
-    
+
     $('#map-select-layer #menu-options ul li.topic-selected').removeClass('topic-selected');
-    
+
     if($(this).attr("id") == "right-topic-button") { var dataVal = rightLayerData; menuViewed = 1; }
     else { var dataVal = leftLayerData; menuViewed = 0; }
-    
+
     $.each($('#map-select-layer #menu-options ul li'), function(key, val){
         if(datasetsArray[$(val).text()] == dataVal){
             $(val).addClass("topic-selected");
             return false;
         }
     });
-    
+
     $('#map-select-layer').show().animate({height: "100%"}, {duration: 750});
 });
 
@@ -480,7 +482,7 @@ function hideTopicMenu(){
         $(this).hide();
     }});
 }
-    
+
 $('#menu-icon').click(function() {
     hideTopicMenu();
 });
@@ -489,14 +491,14 @@ function renderDataSets(){
     $.each(datasetsArray, function(key, val){
         $('#map-select-layer #menu-options ul').append('<li>' + key + '</li>');
     });
-}    
+}
 renderDataSets();
 
 $('#map-select-layer #menu-options ul').on("click", "li", function() {
     if($(this).hasClass('topic-selected')){ hideTopicMenu(); return; }
     $('.topic-selected').removeClass('topic-selected');
     $(this).addClass('topic-selected');
-    
+
     if(menuViewed == 0){
         leftLayerData = datasetsArray[$(this).text()];
         map.removeLayer(leftLayer);
@@ -509,7 +511,7 @@ $('#map-select-layer #menu-options ul').on("click", "li", function() {
         setRightLayer(rightLayerData);
         buttonExpand("right", true);
     }
-    
+
     hideTopicMenu();
     adjustDataContainer();
 });
