f080c9a4789345af26fa15494b6bd87c3f31d77d
Sam Cordingley <samcordingley@gmail.com>
2015-05-06 21:11:27 +0100

    fix geolocation

--- MyEnvironment/www/js/modules/search.js
+++ MyEnvironment/www/js/modules/search.js
@@ -4,7 +4,7 @@ var search = {
     _searchInput: $('#search-input'),
     _expandedDiv: $('#search-bar-expanded'),
     _searchMask: $('#search-mask'),
-    _searchDiv: $('#search-results'), 
+    _searchDiv: $('#search-results'),
     _searchHeader: $('#search-results .expanded-title'),
     _savedHeader: $('#saved-locations .expanded-title'),
     _searchResults: $('#search-results .expanded-locations'),
@@ -12,29 +12,29 @@ var search = {
     _emptySearchDiv: $('#search-bar-empty'),
     _favouritedDiv: $('#search-bar-favourite'),
     _geolocationDiv: $('#search-bar-location'),
-    
+
     _searchDelay: null,
     _currentLocation: undefined,
     _savedLocations: (localStorage['savedLocations'] !== undefined) ? JSON.parse(localStorage['savedLocations']) : [],
-    
+
     init: function(){
-        
+
         var _this = this;
-        
+
         this.renderSavedLocations();
-        
+
         // button listeners
         this._searchInput.on('click', function(){ _this.expandSearch(); });
-        
+
         $('#search-icon').on('click', function(){
             (_this._expandedDiv.is(':visible')) ? _this.collapseSearch() : _this.expandSearch();
         });
-        
+
         $('html').on('click', function() { _this.collapseSearch(); });
-        
+
         this._containerDiv.on('click', function(e){ e.stopPropagation(); });
         this._expandedDiv.on('click', function(e){ e.stopPropagation(); });
-        
+
         // input listener
         this._searchInput.on('input', function() {
 
@@ -42,20 +42,20 @@ var search = {
             _this.setFavouriteIcon(false);
             _this.currentLocation = undefined;
             _this.triggerSearch();
-            
+
         });
-        
+
         this._searchInput.bind("enter", function(){
             if(_this._searchResults.children('li').length == 0) return;
             _this.goToResult(_this._searchResults.children('li').first());
         });
 
         this._searchInput.keyup(function(e){ if(e.keyCode == 13) $(this).trigger("enter"); });
-        
+
         // search/saved result item listener
         this._searchResults.on('click', 'li', function(){ _this.goToResult($(this)); });
         this._savedResults.on('click', 'li', function(){ _this.goToResult($(this)); });
-        
+
         // empty searchbar
         this._emptySearchDiv.on('click', function(){
             _this._searchInput.val("").focus();
@@ -64,10 +64,10 @@ var search = {
             _this.setFavouriteIcon(false);
             _this.expandSearch();
         });
-        
+
         // favourite button listener
         this._favouritedDiv.on('click', function(){ _this.favouriteListener(); });
-        
+
         // delete favourite listener
         this._savedResults.on('click', 'li .favourite-delete', function(e) {
             _this.removeSavedLocation({name: $(this).parent().text() });
@@ -76,19 +76,19 @@ var search = {
 
         // geolocation listener
         this._geolocationDiv.on('click', function() {
-            navigator.geolocation.getCurrentPosition(function(){ _this.geolocationSuccess(); }, function(){ _this.geolocationError(); });
+            navigator.geolocation.getCurrentPosition(function(pos){ _this.geolocationSuccess(pos); }, function(){ _this.geolocationError(); });
         });
-        
+
         //if on a mobile device use the deviceready event, else trigger manually
         (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) ? document.addEventListener("deviceready", this.onDeviceReady, false) : this.onDeviceReady();
 
     },
-    
+
     expandSearch: function(){
-        
+
         // ignore if already open
         if(this._expandedDiv.is(':visible')) return;
-        
+
         this.triggerSearch();
 
         this._searchMask.fadeIn();
@@ -98,45 +98,45 @@ var search = {
             $(this).attr('src', 'img/search/left-arrow-icon.png').fadeIn();
         });
     },
-    
+
     collapseSearch: function(){
-        
+
         // ignore if already closed
         if(!this._expandedDiv.is(':visible')) return;
-        
+
         this._searchMask.fadeOut();
         this._expandedDiv.slideUp();
-        
+
         $('#search-icon img').fadeOut(100, function(){
             $(this).attr('src', 'img/search/search-icon.png').fadeIn();
         });
     },
-    
+
     triggerSearch: function() {
-        
+
         var _this = this;
         var searchQuery = this._searchInput.val();
         this._searchResults.empty();
-   
+
         // no API call if empty
         if(searchQuery == ""){
             this._searchDiv.hide();
             this._emptySearchDiv.hide();
             return;
         }
-        
+
         this._searchDiv.show();
         this._emptySearchDiv.css("display", "inline-block");
-        
+
         //clean commas from query string to optimise processing on nominatim's end
         // searchQuery = inp.val().replace(", ", " ").replace(","," ");
 
         // show loading
         this.updateSearchHeader("loading");
-        
+
         //timer used so fast-typing doesn't trigger rapid requests
         clearTimeout(this._searchDelay);
-        this._searchDelay = setTimeout(function() { 
+        this._searchDelay = setTimeout(function() {
             $.ajax({
                 url: 'http://nominatim.openstreetmap.org/search?format=json&limit=5&countrycodes=gb&q=' + searchQuery,
                 dataType: 'json',
@@ -144,32 +144,32 @@ var search = {
             });
         }, 250);
     },
-    
+
     updateSearchResults: function(data){
-        
+
         // if no data
         if(data === undefined) return;
-        
+
         var _this = this;
         var displayedResults = [];
         this._searchResults.empty();
-        
+
         $.each(data, function(key, val) {
-        
+
             // ignore duplicates
             var parsedName = val.display_name.toLowerCase();
             if($.inArray(parsedName, displayedResults) != -1) return true;
-            
+
             _this._searchResults.append("<li data-lat='" + val.lat + "' data-lon='" + val.lon + "' data-type='" + val.type + "'>" + val.display_name + '</li>');
             displayedResults.push(parsedName);
         });
-        
+
         // update search header
         (displayedResults.length != 0) ? this.updateSearchHeader("results") : this.updateSearchHeader("no-results");
     },
-    
+
     goToResult: function(data){
-        
+
         //update current location
         currentLocation = {
             lat: parseFloat(data.attr('data-lat')),
@@ -187,28 +187,28 @@ var search = {
         else if(currentLocation.type == "city") var newZoom = 12;
         else if(currentLocation.type == "village" || currentLocation.type == "residential" || currentLocation.type == "hamlet") var newZoom = 14;
         else var newZoom = 13;
-        
+
         map.panToLocation(new L.LatLng(currentLocation.lat, currentLocation.lng), newZoom);
-        
-        this.collapseSearch();       
+
+        this.collapseSearch();
         (this.isSaved(currentLocation)) ? this.setFavouriteIcon(true) : this.setFavouriteIcon(false);
     },
-    
+
     updateSearchHeader: function(type){
         if(type == "loading") this._searchHeader.html('<img id="loading-icon" src="img/search/loading.gif" alt="Loading icon"></div>Searching for locations...');
         else if(type == "results") this._searchHeader.text("Search results");
         else this._searchHeader.text("No results found");
     },
-    
+
     updateSavedHeader: function(){
         (this._savedLocations.length > 0) ? this._savedHeader.text("Saved locations") : this._savedHeader.text("No saved locations");
     },
-    
+
     renderSavedLocations: function(){
-    
+
         var _this = this;
         this._savedResults.empty();
-        
+
         $.each(this._savedLocations, function(key, val) {
             _this._savedResults.append("<li data-lat='" + val.lat + "' data-lon='" + val.lng + "' data-type='" + val.type+"'><div>" + val.name +
                 '</div><img class="favourite-delete" src="img/search/favourite-delete-icon.png" alt="Delete favourite" /><div class="clearfix"></div></li>');
@@ -218,36 +218,36 @@ var search = {
     },
 
     favouriteListener: function() {
-    
+
         var currentLocation = this.getCurrentLocation();
         if(currentLocation === undefined) return;
-        
-        (this.isSaved(currentLocation)) ? this.removeSavedLocation(currentLocation) : this.addSavedLocation(currentLocation);  
+
+        (this.isSaved(currentLocation)) ? this.removeSavedLocation(currentLocation) : this.addSavedLocation(currentLocation);
     },
 
     /* accessor functions */
-    
+
     setFavouriteIcon: function(bool){
         bool ? this._favouritedDiv.addClass('favourite-toggled') : this._favouritedDiv.removeClass('favourite-toggled');
     },
-    
-    setCurrentLocation(data){
+
+    setCurrentLocation: function(data){
         this._currentLocation = data;
     },
-    
-    getCurrentLocation(){
+
+    getCurrentLocation: function(){
         return this._currentLocation;
     },
-    
+
     isSaved: function(location){
         for(var i = 0; i < this._savedLocations.length; i++){
             if(location.name == this._savedLocations[i].name) return true;
         }
         return false;
     },
-    
+
     addSavedLocation: function(location){
-        
+
         this._savedLocations.push(location);
         $('#saved-locations .expanded-locations').append("<li data-lat='" + location.lat+"' data-lon='" + location.lng + "' data-type='" + location.type + "'><div>" + location.name +
             '</div><img class="favourite-delete" src="img/search/favourite-delete-icon.png" alt="Delete favourite" /><div class="clearfix"></div></li>');
@@ -256,25 +256,25 @@ var search = {
         this.setFavouriteIcon(true);
         localStorage["savedLocations"] = JSON.stringify(this._savedLocations);
     },
-    
+
     removeSavedLocation: function(location){
-        
+
         for(var i = 0; i < this._savedLocations.length; i++){
-            
+
             if(location.name != this._savedLocations[i].name) continue;
-             
+
             this._savedLocations.splice(i, 1);
             this._savedResults.children('li:eq(' + i + ')').remove();
             if(this.getCurrentLocation() !== undefined && location.name == this.getCurrentLocation().name) this.setFavouriteIcon(false);
             break;
         }
-        
+
         this.updateSavedHeader();
         localStorage["savedLocations"] = JSON.stringify(this._savedLocations);
     },
-    
+
     /* mobile functions */
-    
+
     geolocationSuccess: function(location) {
         map.panToLocation(new L.LatLng(location.coords.latitude, location.coords.longitude));
     },
@@ -282,19 +282,19 @@ var search = {
     geolocationError: function() {
         alert("Sorry, we couldn't find your location!");
     },
-    
+
     // enable the android backbutton and get current position
     onDeviceReady: function() {
         var _this = this;
 
-        navigator.geolocation.getCurrentPosition(function(){ _this.geolocationSuccess(); }, function(){ /* _this.geolocationError(); */ });
+        navigator.geolocation.getCurrentPosition(function(pos){ _this.geolocationSuccess(pos); }, function(){  _this.geolocationError();  });
         document.addEventListener("backbutton", function(){ _this.collapseSearch(); }, false);
-        
+
         // analytics
         if (window.analytics !== undefined){
 			window.analytics.startTrackerWithId('UA-61968992-6');
 		} else {
 			ga('create', 'UA-61968992-6', 'auto');
-		} 
-    }  
+		}
+    }
 }
